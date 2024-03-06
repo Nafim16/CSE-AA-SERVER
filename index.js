0f7f5e494d16b81f38964d1a2f86d535f1010333
Nafim16 <dnafim081999@gmail.com>
2024-03-06 18:10:12 +0600

    cors changed

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 //middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
-
+app.use(cors({
+  origin: 'https://caalu.me'
+}));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wlgklm6.mongodb.net/?retryWrites=true&w=majority`;
 
