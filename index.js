9a5598cb8a2fd3a08a3c19c5b504ec6ff6eedebf
Nafim16 <dnafim081999@gmail.com>
2024-05-11 11:21:27 +0600

    link updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 app.use(express.json());
 
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://caalu.me'],
     credentials: true
 }));
 app.use(cookieParser());
