010a30633c61cb7d666a5e0bf8e4f88e0e86e16d
Nafim16 <dnafim081999@gmail.com>
2024-05-14 01:19:26 +0600

    cors updated

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ require('dotenv').config();
 //middleware
 app.use(cookieParser());
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
-app.use(cors({
-    origin: ['http://localhost:3000', 'https://cse-aa.onrender.com', 'https://cse-aa-server.onrender.com', 'http://cse-aa.vercel.app', 'https://cse-aa-git-main-nafims-projects.vercel.app/', 'https://cse-aa-nafims-projects.vercel.app/'],
-    credentials: true,
-    // origin: ['http://localhost:3000'],
-}));
+// app.use(cors({
+//     origin: ['http://localhost:3000', 'https://cse-aa.onrender.com', 'https://cse-aa-server.onrender.com', 'http://cse-aa.vercel.app', 'https://cse-aa-git-main-nafims-projects.vercel.app/', 'https://cse-aa-nafims-projects.vercel.app/'],
+//     credentials: true,
+//     // origin: ['http://localhost:3000'],
+// }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wlgklm6.mongodb.net/?retryWrites=true&w=majority`;
 
